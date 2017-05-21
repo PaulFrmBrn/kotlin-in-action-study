41f05cfdcde60784d4bae53a51de2066e2495c78
paulfrmbrn <lesspaul@bk.ru>
2017-05-21 19:23:13 +0300

    Chapter 2 Page 41 exceptions

--- chapter2.page40.exceptions/src/main/kotlin/App.kt
+++ chapter2.page40.exceptions/src/main/kotlin/App.kt
@@ -8,6 +8,7 @@ import java.io.StringReader
 fun main(args: Array<String>) {
     println("Hello, world!")
     readNumber(BufferedReader(StringReader("asd")))
+    readNumber2(BufferedReader(StringReader("asd")))
 }
 
 // Kotlin doesn't differentiate between checked and unchecked exceptions
@@ -22,4 +23,17 @@ fun readNumber(reader: BufferedReader): Int? {
         reader.close() // throws *checked* IOException
         // but it is almost always impossible to do anything with it
     }
+}
+
+// more concise syntax
+fun readNumber2(reader: BufferedReader) {
+    val number = try { // try - is an expression
+        // the result of the last expression in the block is the result of the *try*
+        Integer.parseInt(reader.readLine())
+    } catch (e: NumberFormatException) {
+        // the result of the last expression in the block is the result of the *try*
+        //return // in this case *println(number)* will not be called
+        null // in this case number will get the value *null* and *println(number)* will be called
+    }
+    println(number)
 }
