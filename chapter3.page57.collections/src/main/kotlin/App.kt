02db8655287e2df636ae773d5cbae5730382b2d9
PaulFrmBrn <lesspaul@bk.ru>
2017-07-27 19:17:12 +0300

    chapter 3 page 59 varargs

--- chapter3.page57.collections/src/main/kotlin/App.kt
+++ chapter3.page57.collections/src/main/kotlin/App.kt
@@ -4,6 +4,14 @@
 
 fun main(args: Array<String>) {
 
-    println("Hello, collections!")
+    // just an example
+    val strings: List<String> = listOf("first", "second", "fourteenth")
+    println("last from $strings is ${strings.last()}")
+
+    // varargs is almost equals to Java's [...], but Arrays shuold be unpacked before passing as varargs (using *)
+    // this allows to pass additional arguments with unpacked array
+    val array = arrayOf("a","b","c")
+    val anotherList = listOf("another argument 1","another argument 2",*array)
+    println("anotherList is $anotherList")
 
 }
