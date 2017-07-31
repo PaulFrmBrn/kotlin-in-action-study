5c8ad9067b4a295a967404185b88e6016e6163f2
PaulFrmBrn <lesspaul@bk.ru>
2017-07-31 18:34:36 +0300

    chapter 3 page 60 collections

--- chapter3.page57.collections/src/main/kotlin/App.kt
+++ chapter3.page57.collections/src/main/kotlin/App.kt
@@ -14,4 +14,30 @@ fun main(args: Array<String>) {
     val anotherList = listOf("another argument 1","another argument 2",*array)
     println("anotherList is $anotherList")
 
+    // "to" is not a keyword - it is infix function
+    val map1 = mapOf(1 to "one", 7 to "seven", 53 to "fifty-three")
+    println("map1 = $map1")
+    // the same construct
+    val map2 = mapOf(1.to("one"), 7.to("seven"), 53.to("fifty-three"))
+    println("map2 = $map2")
+
+    // using infix function both with destructing declaration - splitting Pair objects into two objects
+    val (num, string) = 1 pair "one"
+    println("num = $num and string = $string")
+
+    // also can be used with Maps
+    println("map:")
+    for ((key,value) in map1) {
+        println("key = $key, value = $value")
+    }
+    // and collections
+    println("collection:")
+    for ((index,element) in strings.withIndex()) {
+        println("index = $index, element = $element")
+    }
+
 }
+
+// infix modifier specifies infix function
+// infix can be used for any method that have one required argument
+infix fun Any.pair(other: Any) = Pair(this,other)
