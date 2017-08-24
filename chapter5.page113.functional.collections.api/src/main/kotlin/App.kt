cf18c0f296c36edbffbfe6850277784aef658e7f
paulfrmbrn <lesspaul@bk.ru>
2017-08-24 22:10:23 +0300

    chapter 5 page 119 functional collections api

--- chapter5.page113.functional.collections.api/src/main/kotlin/App.kt
+++ chapter5.page113.functional.collections.api/src/main/kotlin/App.kt
@@ -2,11 +2,48 @@
  * @author PaulFrmBrn
  */
 fun main(args: Array<String>) {
+
     println("functional APIs for collections")
 
-    // doesn't change the collection new collection as a result
-    println(listOf(1,2,3,4).filter{ it % 2 == 0 })
-    println(listOf(Person("Alice",29),Person("Bob",31)).filter { it.age > 30 })
+    val ints = listOf(1, 2, 3, 4)
+    val people = listOf(Person("Alice", 29), Person("Bob", 31))
+
+    // filtering
+    // doesn't change the collection -  new collection as a result
+    println(ints.filter{ it % 2 == 0 })
+    println(people.filter { it.age > 30 })
+
+    // mapping (transformation)
+    println(people.map { it.name })
+    println(people.map(Person::name)) // member reference
+
+    // can be chained
+    println(people.filter { it.age > 30 }.map { it.name} ) // NB each call leads to new collection creation (not a stream)
+
+    // maximum
+    val maxAge = people.maxBy(Person::age)?.age
+    people.filter { it.age == maxAge }
+
+    // special cases for maps
+    val pairs = mapOf(0 to "zero", 1 to "one")
+    println(pairs.mapValues { it.value.toUpperCase() }.filterValues { it.length > 3 })
+    println(pairs.mapKeys { it.key * 2 }.filterKeys { it > 0 })
+
+    val canBeInClub29 = { p:Person -> p.age <= 29}
+
+    // all elements satisfy predicate
+    println(people.all(canBeInClub29))
+
+    // at least one element satisfy predicate
+    println(people.any(canBeInClub29))
+
+    val listOfTree = listOf(1,2,3)
+    println(!listOfTree.all { it == 3 })
+    println(listOfTree.any { it != 3 }) // this is preferable - negotiation is noticeable
+
+    // count of elements that satisfy predicate
+    println(people.count(canBeInClub29)) // no intermediate collection created
+    println(people.filter(canBeInClub29).size) // intermediate collection created
 
 }
 
