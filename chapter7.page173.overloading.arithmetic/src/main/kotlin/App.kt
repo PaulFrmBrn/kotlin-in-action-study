ba550a390fcf539602282d8faca97ca1fd4c1a1b
paulfrmbrn <lesspaul@bk.ru>
2017-09-07 23:15:40 +0300

    chapter 6 page 173 overloading arithmetci operations

--- chapter7.page173.overloading.arithmetic/src/main/kotlin/App.kt
+++ chapter7.page173.overloading.arithmetic/src/main/kotlin/App.kt
@@ -14,6 +14,11 @@ fun main(args: Array<String>) {
     println(p1.plus(p2)) // same effect
     // binary operators 'plus +', 'minus -', 'mod %', 'div /', 'times *' can be overloaded
     // precedence is the same as for numeric operations
+    println(p1 * 1.5)
+    println(2.0 * p1)
+    println('a' * 3)
+    // operator functions can be overloaded
+    // there is no special operators fo bitwise operations -infix functions are used instead
 
 }
 
@@ -29,4 +34,18 @@ data class Point(val x: Int, val y: Int) {
 operator fun Point.plus(other: Point): Point { // extension functions is shadowed by member function
     println("plus extension function")
     return Point(this.x + other.x, this.y + other.y)
+}
+
+operator fun Point.times(scale: Double): Point { // operand don't have to be of the same type
+    println("Point times Double operation")
+    return Point((this.x * scale).toInt(),(this.y * scale).toInt())
+}
+
+operator fun Double.times(point: Point): Point { // there is no commutativity out of the box
+    println("Double times Point operation") // opposite operation should be implemented
+    return Point((point.x * this).toInt(),(point.y * this).toInt())
+}
+
+operator fun Char.times(count: Int): String { // return type may differ from either of operands
+    return this.toString().repeat(count)
 }
